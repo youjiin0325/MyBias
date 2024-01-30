ffb82080183a7cf11e7dd3e6b1823939b90a3a9b
youjiin0325 <youjiin0325@gmail.com>
2024-01-30 15:43:51 +0900

    Feat: Json 데이터 디코딩후 , 애니추천 리스트 뷰에서 보이는지 확인

---
+++ MyBias/MyBias/animeAPI/AnimeCategoryHome.swift
@@ -0,0 +1,18 @@
+//
+//  AnimeCategoryHome.swift
+//  MyBias
+//
+//  Created by 유지인 on 1/30/24.
+//
+
+import SwiftUI
+
+struct AnimeCategoryHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimeCategoryHome()
+}
