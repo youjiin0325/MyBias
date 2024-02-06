6881ef48968fb73d7763996192f769e74c097e98
youjiin0325 <youjiin0325@gmail.com>
2024-02-06 16:23:15 +0900

    Style: 마이페이지 디자인정리, 메인색상 변경, - 마이페이지 게시글을 위한 Json 데이터 생성, 구조체 생성

---
+++ MyBias/MyBias/내페이지/Content.swift
@@ -0,0 +1,8 @@
+//
+//  Content.swift
+//  MyBias
+//
+//  Created by 유지인 on 2/6/24.
+//
+
+import Foundation
---
+++ MyBias/MyBias/내페이지/MyProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  MyProfileView.swift
+//  MyBias
+//
+//  Created by 유지인 on 2/6/24.
+//
+
+import SwiftUI
+
+struct MyProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyProfileView()
+}
