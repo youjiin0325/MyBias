8a2c5d96953486b4f3e41f1a41f0650eb7794836
youjiin0325 <youjiin0325@gmail.com>
2024-02-14 17:00:53 +0900

    Feat: 메인 Home 화면 뷰 생성 - 덕질중 섹션: 내가 올린 글 (최근)이미지, 내 프로필 사진, 내가 관심있는 태그 -네취향: 다양한 취향의 작품 추천 (랜덤) -이세계 커뮤니티: 가장 최근 게시물 위주로 보여주는 섹션

---
+++ MyBias/MyBias/HOME/RealtimeCoummunity.swift
@@ -0,0 +1,18 @@
+//
+//  RealtimeCoummunity.swift
+//  MyBias
+//
+//  Created by 유지인 on 2/14/24.
+//
+
+import SwiftUI
+
+struct RealtimeCoummunity: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RealtimeCoummunity()
+}
---
+++ MyBias/MyBias/HOME/WhosFavourite.swift
@@ -0,0 +1,18 @@
+//
+//  WhosFavourite.swift
+//  MyBias
+//
+//  Created by 유지인 on 2/14/24.
+//
+
+import SwiftUI
+
+struct WhosFavourite: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WhosFavourite()
+}
