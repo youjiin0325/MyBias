6e128ebc8c881987096baa09d785e7d622e86f7c
youjiin0325 <youjiin0325@gmail.com>
2024-02-15 17:19:21 +0900

    Feat: AddNewPost 새로운 글 올리는 뷰 생성, 사진(여러장 가능)도 업로드 할 수 있음(PhotoPicker)

---
+++ MyBias/MyBias/내페이지/AddNewPostView.swift
@@ -0,0 +1,18 @@
+//
+//  AddNewPostView.swift
+//  MyBias
+//
+//  Created by 유지인 on 2/15/24.
+//
+
+import SwiftUI
+
+struct AddNewPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddNewPostView()
+}
