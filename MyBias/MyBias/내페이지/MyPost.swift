0830ace5df16ef04a79a07a1b40fbe41411c9ba4
youjiin0325 <youjiin0325@gmail.com>
2024-02-19 16:51:02 +0900

    Error: addNewPost 버튼 클릭 후 메인으로 돌아가지 않음(Path is empy)

---
+++ MyBias/MyBias/내페이지/MyPost.swift
@@ -0,0 +1,18 @@
+//
+//  MyPost.swift
+//  MyBias
+//
+//  Created by 유지인 on 2/19/24.
+//
+
+import SwiftUI
+
+struct MyPost: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyPost()
+}
